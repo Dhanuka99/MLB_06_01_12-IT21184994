d500c342396cf9c2d3017a1d432229659d025b77
DHANUKA <danuka1999lakshan@gmail.com>
2022-05-20 23:30:35 +0530

    MLB_06_01_12 IT21184994

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Employee.h"
+#include "Administrator.h"
+#include "AreaManager.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	return 0;
+}
