8e422a43a6764972a59910a842fc678f6becad96
DHANUKA <danuka1999lakshan@gmail.com>
2022-05-20 23:49:44 +0530

    MLB_06_01_12 IT21184994

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	
+
+	AreaManager *p = new AreaManager("Dhanuka","Kurunegala",001,"danu","danu1234","areaManager");
+	p->displayDetails();
 	return 0;
 }
