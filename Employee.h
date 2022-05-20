65dd997dec3af1c69f4ea0aabb9272f1ac2165c0
DHANUKA <danuka1999lakshan@gmail.com>
2022-05-20 23:53:31 +0530

    MLB_06_01_12 IT21184994

--- Employee.h
+++ Employee.h
@@ -1,3 +1,4 @@
+
 class Employee{
 protected:
 	int empID;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Employee.h"
 #include "Administrator.h"
 #include "AreaManager.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -8,5 +7,9 @@ int main() {
 
 	AreaManager *p = new AreaManager("Dhanuka","Kurunegala",001,"danu","danu1234","areaManager");
 	p->displayDetails();
+	p->~AreaManager();
+	Administrator *a = new Administrator("saman",002,"saman_1","saman123","admin");
+	a->displayDetails();
+	a->~Administrator();
 	return 0;
 }
